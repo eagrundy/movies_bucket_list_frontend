4c5cc76307fc7f5fc11e1f8ccaa5919a0002abe3
eagrundy <ericamalakian@gmail.com>
2021-05-12 21:29:42 -0700

    Fixed contact component

--- src/components/header/Contact.js
+++ src/components/header/Contact.js
@@ -13,9 +13,9 @@ function Contact() {
         <br />
         <div className="social">
         Follow us and send us a message on our social media:<br /><br />
-        <img src="twitter.png" id="twitter" onClick={() => window.open( `https://www.twitter.com`)} />
-        <img src="facebook.png" id="facebook" onClick={() => window.open( `https://www.facebook.com`)} />
-        <img src="github.jpg" id="github" onClick={() => window.open( `https://github.com/eagrundy/movies_bucket_list_frontend`)} />
+        <img src="twitter.png" alt="twitter" onClick={() => window.open( `https://www.twitter.com`)} />
+        <img src="facebook.png" alt="facebook" onClick={() => window.open( `https://www.facebook.com`)} />
+        <img src="github.jpg" alt="github" onClick={() => window.open( `https://github.com/eagrundy/movies_bucket_list_frontend`)} />
         <br /><br /><br /><br />
         </div>
     </div>
